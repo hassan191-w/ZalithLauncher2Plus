d71dbad8a045416eef3fc5bf4d8a00a73182280c
hassan191-w <hassanzahran252@gmail.com>
2026-09-25 16:51:10 +0300

    Create Constants.kt

---
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/activities/Constants.kt
@@ -0,0 +1,8 @@
+package com.movtery.zalithlauncher.ui.activities
+
+const val EXTRA_LAUNCH_VERSION = "extra_launch_version"
+const val EXTRA_OPEN_LOG = "extra_open_log"
+const val EXTRA_IMPORT_TYPE = "extra_import_type"
+const val EXTRA_IMPORT_URI = "extra_import_uri"
+const val IMPORT_TYPE_MODPACK = "modpack"
+const val IMPORT_TYPE_CONTROLS = "controls"
