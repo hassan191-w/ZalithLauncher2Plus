612c9a6d25181773c53829cc49c53fcd1305c591
hassan191-w <hassanzahran252@gmail.com>
2026-09-25 16:52:19 +0300

    Update SplashActivity.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/activities/SplashActivity.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/activities/SplashActivity.kt
@@ -1,21 +1,3 @@
-/*
- * Zalith Launcher 2
- * Copyright (C) 2025 MovTery <movtery228@qq.com> and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
- */
-
 package com.movtery.zalithlauncher.ui.activities
 
 import android.content.Intent
@@ -30,12 +12,6 @@ import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import com.movtery.zalithlauncher.R
-
-// ✅ جميع الثوابت المطلوبة للمشروع
-const val EXTRA_LAUNCH_VERSION = "extra_launch_version"
-const val EXTRA_OPEN_LOG = "extra_open_log"
-const val IMPORT_TYPE_MODPACK = "modpack"
-const val IMPORT_TYPE_CONTROLS = "controls"
 
 class SplashActivity : AppCompatActivity() {
 
