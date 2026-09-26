7cbf8ddc6963d0aeb9fbee3e69f860aaf54be3b7
hassan191-w <hassanzahran252@gmail.com>
2026-09-26 17:37:44 +0300

    Create YouTubeVideo.kt

---
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/youtube/YouTubeVideo.kt
@@ -0,0 +1,9 @@
+package com.movtery.zalithlauncher.game.youtube
+
+data class YouTubeVideo(
+    val id: String,
+    val title: String,
+    val published: String,
+    val thumbnailUrl: String,
+    val videoUrl: String
+)
