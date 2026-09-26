86f6a5b31535054cda80a1d73a37f02e9ee94012
hassan191-w <hassanzahran252@gmail.com>
2026-09-26 17:55:13 +0300

    Update SideBar.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/elements/SideBar.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/elements/SideBar.kt
@@ -89,6 +89,7 @@ fun SideBar(
     isVisible: Boolean,
     onFpsClick: () -> Unit,
     onVersionsClick: () -> Unit,
+    onMyVideosClick: () -> Unit,
     onInfoClick: () -> Unit
 ) {
     var expanded by rememberSaveable { mutableStateOf(false) }
@@ -134,6 +135,7 @@ fun SideBar(
                     expanded = expanded,
                     onFpsClick = onFpsClick,
                     onVersionsClick = onVersionsClick,
+                    onMyVideosClick = onMyVideosClick,
                     onInfoClick = onInfoClick,
                     modifier = Modifier.align(Alignment.TopCenter)
                 )
@@ -152,6 +154,7 @@ private fun SideBarMenuContent(
     expanded: Boolean,
     onFpsClick: () -> Unit,
     onVersionsClick: () -> Unit,
+    onMyVideosClick: () -> Unit,
     onInfoClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -196,6 +199,14 @@ private fun SideBarMenuContent(
                     icon = painterResource(R.drawable.ic_assignment_filled),
                     label = stringResource(R.string.page_title_version_manage),
                     onClick = onVersionsClick
+                )
+            }
+
+            StaggeredItem(delay = 150) {
+                SideBarShortcut(
+                    icon = painterResource(R.drawable.ic_video_settings),
+                    label = stringResource(R.string.my_videos),
+                    onClick = onMyVideosClick
                 )
             }
 
